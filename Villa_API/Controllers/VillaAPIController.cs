a49e2513874949b442dc023bc2c2d013eaf115d0
astrophie <sophie.w.feng@gmail.com>
2023-07-07 22:14:38 -0700

    second commit

---
+++ Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa_API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Villa_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id=1, Name = "Pool View"},
+                new Villa {Id=2, Name = "Beach View"}
+            };
+        }
+    }
+}
+
---
+++ Villa_API/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Villa_API.Models
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
